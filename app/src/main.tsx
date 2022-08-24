0165865b0ff7468600e44aa248f8bcbf0a2769dd
Amy Dang <kirontoo@gmail.com>
2022-08-23 17:09:46 -0700

    feat(main.tsx): set up routing

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,13 +3,18 @@ import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),

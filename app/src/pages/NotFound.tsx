6340ac610846ba95465cf229de0174c9efaa2003
Amy Dang <kirontoo@gmail.com>
2022-08-30 16:23:51 -0700

    feat(notfound): create 404 not found page

---
+++ app/src/pages/NotFound.tsx
@@ -0,0 +1,33 @@
+import { Box, Button, Heading, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+  return (
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h2"
+        size="2xl"
+        bgGradient="linear(to-r, primary.400, primary.600)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2}>
+        Page Not Found
+      </Text>
+      <Text color={'gray.500'} mb={6}>
+        The page you&apos;re looking for does not seem to exist
+      </Text>
+
+      <Button
+        colorScheme="primary"
+        bgGradient="linear(to-r, primary.400, primary.500, primary.600)"
+        color="white"
+        variant="solid"
+      >
+        <Link to="/">Go to Home</Link>
+      </Button>
+    </Box>
+  );
+}

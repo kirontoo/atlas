344eed29fc1566f8444b8e188053a00258fc4598
Amy Dang <kirontoo@gmail.com>
2022-08-30 16:38:41 -0700

    style(theme): lighten background color of light mode

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -9,7 +9,7 @@ const styles = {
     body: {
       fontFamily: 'body',
       color: mode('gray.800', 'whiteAlpha.900')(props),
-      bg: mode('gray.100', 'gray.800')(props),
+      bg: mode('gray.50', 'gray.800')(props),
       lineHeight: 'base',
     },
     '*::placeholder': {

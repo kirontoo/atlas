42098348c748e0b9318371185a529f0be1878ce4
Amy Dang <kirontoo@gmail.com>
2022-10-07 19:48:38 -0700

    feat: add endpoint to create a new ticket

--- server/main.go
+++ server/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/kirontoo/atlas/server/internals/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const projectDirName = "server"
+const database = "atlas"
+
+var db *mongo.Client
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -29,7 +39,8 @@ func CORSMiddleware() gin.HandlerFunc {
 }
 
 type api struct {
-	router *gin.Engine
+	router  *gin.Engine
+	tickets *models.TicketModel
 }
 
 func main() {
@@ -45,24 +56,29 @@ func main() {
 		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
 
+	db = getMongoClient(uri)
+
 	gin.ForceConsoleColor()
 	api := &api{
-		router: gin.Default(),
+		router:  gin.New(),
+		tickets: &models.TicketModel{DB: db, Collection: getCollection("tickets", db)},
 	}
-
-	// db := getMongoClient(uri)
 
 	// set middlewares
 	api.router.Use(CORSMiddleware())
 
-	api.router.GET("/ping", func(c *gin.Context) {
+	api.router.POST("/api/tickets", api.CreateTicket)
 
+	api.router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-	api.router.Run(":" + port)
+	err := api.router.Run(":" + port)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func loadEnv() {
@@ -75,4 +91,57 @@ func loadEnv() {
 	if err != nil {
 		log.Fatalf("Error loading .env file")
 	}
+}
+
+func getMongoClient(uri string) *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	} else {
+		log.Println("this is working!")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connected to database")
+	}
+
+	return client
+}
+
+func (s *api) CreateTicket(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	var ticket *models.Ticket
+
+	if err := c.BindJSON(&ticket); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	ticket.ID = primitive.NewObjectID()
+	ticket.CreatedBy = nil
+	ticket.CreatedAt = primitive.NewDateTimeFromTime(time.Now())
+	ticket.UpdatedAt = ticket.CreatedAt
+
+	result, err := s.tickets.Insert(ctx, ticket)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "order was not created"})
+		fmt.Println(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+func getCollection(collectionName string, db *mongo.Client) (collection *mongo.Collection) {
+	collection = db.Database(database).Collection(collectionName)
+	return
 }

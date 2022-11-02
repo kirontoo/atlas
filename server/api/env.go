93713ff617198048d65bc0e1692420aa3464ccef
Amy Dang <kirontoo@gmail.com>
2022-11-01 21:28:50 -0700

    feat: Add secret key environment variable

--- server/api/env.go
+++ server/api/env.go
@@ -11,11 +11,13 @@ import (
 type config struct {
 	port       string
 	mongodbUri string
+	secretKey  string
 }
 
 func (c *config) LoadVariables() {
 	defaultPort := "8000"
 	defaultUri := "mongodb://mongodb0.example.com:27017"
+	defaultSecretKey := "ATLAS_SECRET_SHHHH"
 
 	envPort, ok := os.LookupEnv("ATLAS_PORT")
 	if ok {
@@ -25,14 +27,20 @@ func (c *config) LoadVariables() {
 	if ok {
 		defaultUri = envUri
 	}
+	envSecretKey, ok := os.LookupEnv("ATLAS_SECRET_KEY")
+	if ok {
+		defaultSecretKey = envSecretKey
+	}
 
 	port := flag.String("port", defaultPort, "HTTP network address")
 	uri := flag.String("uri", defaultUri, "Mongo db uri address. dsn should match format: username:password@protocol(address)/dbname?param=value")
+	secretKey := flag.String("uri", defaultSecretKey, "JWT Secret Key")
 
 	flag.Parse()
 
 	c.port = *port
 	c.mongodbUri = *uri
+	c.secretKey = *secretKey
 }
 
 func (c *config) LoadEnv(path string) error {

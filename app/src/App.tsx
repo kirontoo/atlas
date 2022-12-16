58bbb5babd4ff5b31f8b3ff5dbe710cb7d05d937
Amy Dang <kirontoo@gmail.com>
2022-12-15 21:52:30 -0800

    feat(auth): load user login session on app load

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,38 +1,46 @@
 import './App.css';
 
-import { ChakraProvider } from '@chakra-ui/react';
-import { Provider } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { auth } from './auth/firebase';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Dashboard/Projects';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import SignUp from './pages/SignUp';
-import Store from './store';
-import theme from './theme';
+import { login, logout } from './store/features/user/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        dispatch(login(userAuth));
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
-    <ChakraProvider theme={theme}>
-      <Provider store={Store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />}>
-              <Route path="" element={<div> hello home</div>} />
-              <Route path="projects" element={<Projects />} />
-              <Route path="tickets" element={<div>tickets</div>} />
-              <Route path="settings" element={<div>settings</div>} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </ChakraProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="" element={<div> hello home</div>} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="tickets" element={<div>tickets</div>} />
+          <Route path="settings" element={<div>settings</div>} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,13 +1,21 @@
 import './index.css';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import Store from './store';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <Provider store={Store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );

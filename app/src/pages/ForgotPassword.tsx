c21b12d1b39290d58f63b5bd1c7b636c8f93e2e4
Amy Dang <kirontoo@gmail.com>
2023-01-09 19:30:30 -0800

    feat: create form to send reset password email

--- app/src/pages/ForgotPassword.tsx
+++ app/src/pages/ForgotPassword.tsx
@@ -1,5 +1,138 @@
-function ForgotPassword() {
-  return <h1>forgot my password.</h1>;
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react';
+import { FirebaseError } from '@firebase/util';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { FormEvent, useRef, useState } from 'react';
+
+import { auth } from '../utils/firebase';
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+
+  async function sendPasswordResetRequest(e: FormEvent) {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      if (email != '') {
+        await sendPasswordResetEmail(auth, email);
+        onOpen();
+      }
+    } catch (e: unknown) {
+      if (e instanceof FirebaseError) {
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        setError(e.message);
+      }
+      // ..
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}
+      >
+        <Stack
+          spacing={4}
+          w={'full'}
+          maxW={'sm'}
+          bg={useColorModeValue('white', 'gray.700')}
+          rounded={'xl'}
+          boxShadow={'lg'}
+          p={6}
+          my={10}
+        >
+          <Center>
+            <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+              Forgot your password?
+            </Heading>
+          </Center>
+          <VStack
+            fontSize={{ base: 'sm', sm: 'md' }}
+            color={useColorModeValue('gray.800', 'gray.400')}
+            display="flex"
+            flexDirection="column"
+            as="form"
+            onSubmit={sendPasswordResetRequest}
+            gap="1rem"
+          >
+            <FormControl id="email" isInvalid={error !== ''} isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              {error != '' && <FormErrorMessage>{error}</FormErrorMessage>}
+            </FormControl>
+            <Button width="full" type="submit" isLoading={isLoading}>
+              Send Reset Password Email
+            </Button>
+          </VStack>
+        </Stack>
+      </Flex>
+      <SendEmailDialog cancelRef={cancelRef} onClose={onClose} isOpen={isOpen} />
+    </>
+  );
 }
 
-export default ForgotPassword;
+function SendEmailDialog({ cancelRef, onClose, isOpen }) {
+  return (
+    <>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>Reset Password</AlertDialogHeader>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+            An reset password link has been sent to your email! Please click on the link
+            to reset your password.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              Ok
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+}

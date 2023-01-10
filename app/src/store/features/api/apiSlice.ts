72603cdebcc864e1d374be1d0add8db88e29d554
Amy Dang <kirontoo@gmail.com>
2023-01-09 17:47:00 -0800

    feat: API call for user signup

--- app/src/store/features/api/apiSlice.ts
+++ app/src/store/features/api/apiSlice.ts
@@ -1,5 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+interface APIResponse {
+  status: string;
+  data?: Record<string, unknown>;
+  message: string;
+}
+
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
@@ -7,6 +13,22 @@ export const apiSlice = createApi({
     getPing: builder.query<string, void>({
       query: () => '/ping',
       transformResponse: (response: { message: string }) => response.message,
+    }),
+    // TODO: create a type for the body
+    userSignup: builder.query<APIResponse, { token: string; body: any }>({
+      query: (data) => {
+        const { token, body } = data;
+        return {
+          url: '/api/users',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: body,
+        };
+      },
+      transformResponse: (response: { token: string; body: any }) => response.body,
     }),
   }),
 });

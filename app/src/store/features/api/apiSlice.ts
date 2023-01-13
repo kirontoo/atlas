d0ca945814c0f66c3e9307c1e9a16a42e0e070be
Amy Dang <kirontoo@gmail.com>
2023-01-12 19:48:50 -0800

    feat: endpoints to create & update user

--- app/src/store/features/api/apiSlice.ts
+++ app/src/store/features/api/apiSlice.ts
@@ -1,34 +1,56 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-interface APIResponse {
+export const APIBaseURL = import.meta.env.DEV
+  ? 'http://localhost:8080'
+  : import.meta.env.BASE_URL;
+
+interface APIResponse<T> {
   status: string;
-  data?: Record<string, unknown>;
+  data: T;
   message: string;
+}
+
+interface UserResponse {
+  username: string;
+  email: string;
+  role?: number;
+  firstName: string;
+  lastName: string;
 }
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080' }),
+  baseQuery: fetchBaseQuery({ baseUrl: APIBaseURL }),
   endpoints: (builder) => ({
     getPing: builder.query<string, void>({
       query: () => '/ping',
       transformResponse: (response: { message: string }) => response.message,
     }),
-    createUser: builder.mutation<APIResponse, { token: string; body: any }>({
+    createUser: builder.mutation<APIResponse<UserResponse>, any>({
       query: (data) => {
-        const { token, body } = data;
         return {
           url: '/api/users',
           method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-          body: body,
+          body: data,
         };
       },
-      transformResponse: (response: { token: string; body: any }) => response.body,
+      transformResponse: (response: { data: APIResponse<UserResponse> }) => response.data,
     }),
+    updateUser: builder.mutation<APIResponse<UserResponse>, { token: string; body: any }>(
+      {
+        query: ({ token, body }) => {
+          return {
+            url: '/api/users',
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+            body: body,
+          };
+        },
+      },
+    ),
   }),
 });
 

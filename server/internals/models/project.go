fc19326b562675f16491e3e3b4f61b6b79c0e97b
Amy Dang <kirontoo@gmail.com>
2023-01-17 20:00:02 -0800

    feat: db handler for projects

---
+++ server/internals/models/project.go
@@ -0,0 +1,155 @@
+package models
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"runtime/debug"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Project struct {
+	ID          primitive.ObjectID  `bson:"_id,omitempty"         json:"_id"`
+	Title       string              `bson:"title,omitempty"       json:"title"`
+	ProjectHead primitive.ObjectID  `bson:"projectHead,omitempty" json:"projectHead,omitempty"`
+	Description string              `bson:"description,omitempty" json:"description,omitempty"`
+	CreatedBy   primitive.ObjectID `bson:"createdBy,omitempty"   json:"createdBy"`
+	Deadline    primitive.DateTime `bson:"deadline,omitempty"    json:"deadline,omitempty"`
+	CreatedAt   primitive.DateTime  `bson:"createdAt,omitempty"   json:"createdAt,omitempty"`
+	UpdatedAt   primitive.DateTime  `bson:"updatedAt,omitempty"   json:"updatedAt,omitempty"`
+}
+
+type ProjectModel struct {
+	collection *mongo.Collection
+}
+
+func (m *ProjectModel) Init(db *mongo.Database) error {
+	const collectionName = "projects"
+	if db == nil {
+		return errors.New("database does not exist")
+	}
+
+	collectionNames, err := db.ListCollectionNames(context.TODO(), bson.D{})
+	if err != nil {
+		log.Output(2, fmt.Sprintf("%s\n%s", err.Error(), debug.Stack()))
+		return err
+	}
+
+	if !isStringExists(collectionNames, collectionName) {
+		validator := bson.M{
+			"$jsonSchema": m.jsonSchema(),
+		}
+
+		opts := options.CreateCollection().SetValidator(validator)
+
+		err := db.CreateCollection(context.TODO(), "users", opts)
+		if err != nil {
+			log.Output(2, fmt.Sprintf("%s\n%s", err.Error(), debug.Stack()))
+			return err
+		}
+	}
+
+	m.collection = db.Collection(collectionName)
+
+	return nil
+}
+
+func (m *ProjectModel) jsonSchema() bson.M {
+	return bson.M{
+		"bsonType": "object",
+		"required": []string{"title", "createdBy"},
+		"properties": bson.M{
+			"title": bson.M{
+				"bsonType": "string",
+				"description": "the title of the project, which is required and must be a string",
+			},
+			"createdBy": bson.M{
+				"bsonType":    "objectId",
+				"description": "ID reference of the user that created this project, which is required and must be a string",
+			},
+			"projectHead": bson.M{
+				"bsonType":    "objectId",
+				"description": "ID reference of the user that is in charge of the project, which is required and must be a string",
+			},
+			"description": bson.M{
+				"bsonType": "string",
+				"description": "the description of the project, which is required and must be a string",
+			},
+			"deadline": bson.M{
+				"bsonType": "date",
+				"description": "the date of when this project is due",
+			},
+		},
+	}
+}
+
+func (m *ProjectModel) Insert(ctx context.Context, t *Project) (*mongo.InsertOneResult, error) {
+	t.ID = primitive.NewObjectID()
+	t.CreatedAt = primitive.NewDateTimeFromTime(time.Now())
+	t.UpdatedAt = t.CreatedAt
+
+	result, err := m.collection.InsertOne(ctx, t)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (m *ProjectModel) Get(ctx context.Context, id string) (*Project, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Printf("oid %v, id %v", oid, id)
+		return nil, err
+	}
+	var result *Project
+
+	err = m.collection.FindOne(ctx, bson.D{{Key: "_id", Value: oid}}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, ErrNoRecord
+		}
+	}
+	return result, nil
+}
+
+func (m *ProjectModel) UpdateOne(ctx context.Context, id string, project bson.M) (int64, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, err
+	}
+	filter := bson.D{{Key: "_id", Value: oid}}
+	project["updatedAt"] = primitive.NewDateTimeFromTime(time.Now())
+
+	doc, err := toBsonDocument(project)
+	if err != nil {
+		return 0, err
+	}
+	update := bson.D{{Key: "$set", Value: doc}}
+
+	result, err := m.collection.UpdateOne(ctx, filter, update, nil)
+	if err != nil || result.MatchedCount == 0 {
+		return 0, err
+	}
+
+	return result.MatchedCount, nil
+}
+
+func (m *ProjectModel) Delete(ctx context.Context, id string) (int64, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return 0, err
+	}
+
+	res, err := m.collection.DeleteOne(context.TODO(), bson.M{"_id": oid}, nil)
+	if err != nil {
+		log.Print(err)
+		return 0, err
+	}
+	return res.DeletedCount, nil
+}

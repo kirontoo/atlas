13bc9c9b6a723d83836ad4ed5d68c538c8bbd8c0
Amy Dang <kirontoo@gmail.com>
2023-01-26 15:11:42 -0800

    feat: add project state

---
+++ app/src/store/features/projects/index.ts
@@ -0,0 +1,81 @@
+/* eslint-disable no-unused-vars */
+
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { Project } from '../../../types';
+import { RootState } from '../../reducer';
+
+export interface ProjectState {
+  loading: boolean;
+  projects: Project[];
+  type?: string;
+}
+
+const initialState: ProjectState = {
+  loading: false,
+  projects: [],
+};
+
+enum ProjectsActions {
+  LOADING = 'projects/loading',
+  NOT_LOADING = 'projects/not_loading',
+  UPDATED = 'projects/updated',
+  ADDED = 'projects/added',
+  DELETED = 'projects/deleted',
+}
+
+const projectSlice = createSlice({
+  name: 'projects',
+  initialState: initialState,
+  reducers: {
+    loading(state: ProjectState) {
+      return {
+        ...state,
+        type: ProjectsActions.LOADING,
+        loading: true,
+      };
+    },
+
+    notLoading(state: ProjectState) {
+      return {
+        ...state,
+        type: ProjectsActions.NOT_LOADING,
+        loading: false,
+      };
+    },
+
+    update(state: ProjectState, action: PayloadAction<Project[]>) {
+      return {
+        ...state,
+        type: ProjectsActions.UPDATED,
+        projects: [...state.projects, ...action.payload],
+      };
+    },
+
+    add(state: ProjectState, action: PayloadAction<Project[]>) {
+      return {
+        ...state,
+        type: ProjectsActions.DELETED,
+        projects: [...state.projects, ...action.payload],
+      };
+    },
+
+    delete(state: ProjectState, action: PayloadAction<Project[]>) {
+      return {
+        ...state,
+        type: ProjectsActions.DELETED,
+        projects: [...state.projects, ...action.payload],
+      };
+    },
+  },
+});
+
+export const selectProjects = (state: RootState) => state.projects.projects ?? [];
+const selectProjectsId = (_: RootState, projectId: string) => projectId;
+
+export const selectProjectById = createSelector(
+  [selectProjects, selectProjectsId],
+  (projects, projectId) => projects.find((p: Project) => p.id == projectId),
+);
+
+export default projectSlice.reducer;
--- app/src/store/reducer.ts
+++ app/src/store/reducer.ts
@@ -1,14 +1,17 @@
 import { combineReducers } from 'redux';
 
 import { apiSlice } from './features/api/apiSlice';
+import projectReducer, { ProjectState } from './features/projects';
 import userReducer, { IAuthState } from './features/user/userSlice';
 
 export interface RootState {
   user: IAuthState;
+  projects: ProjectState;
 }
 
 const rootReducer = combineReducers({
   user: userReducer,
+  projects: projectReducer,
   [apiSlice.reducerPath]: apiSlice.reducer,
 });
 
